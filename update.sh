162c8e76060a2d5d99c8751d0a41e4916db9691a
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-25 13:00:49 +0300

    chore: add update script

---
+++ update.sh
@@ -0,0 +1,6 @@
+cp ~/.config/polybar/config polybar/config
+cp ~/.config/bspwm/bspwmrc bspwm/bspwmrc
+cp ~/.config/sxhkd/sxhkdrc sxhkd/sxhkdrc
+cp ~/.zshrc .zshrc
+cp ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
+cp ~/.vimrc vim/.vimrc
