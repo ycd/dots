f800256663cb3e35e1f9b2984b57f40e3c009c07
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-25 14:16:16 +0300

    chore: add the update setup file

---
+++ vim/setup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/sh
+
+
+
+# vim-rooter
+git clone https://github.com/airblade/vim-rooter.git ~/.vim/pack/plugins/start/vim-rooter
+
+# fzf-vim 
+git clone https://github.com/junegunn/fzf.vim ~/.vim/pack/plugins/start/fzf.vim
+
+# nerd-tree
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+
+# vim-polyglot 
+git clone --depth 1 https://github.com/sheerun/vim-polyglot ~/.vim/pack/plugins/start/vim-polyglot
+
+# lightline 
+git clone https://github.com/itchyny/lightline.vim ~/.vim/pack/plugins/start/lightline
+
+# Setup coc
+mkdir -p ~/.vim/pack/coc/start
+curl --fail -L https://github.com/neoclide/coc.nvim/archive/release.tar.gz|tar xzfv ~/.vim/pack/coc/start
+
+# :CocInstall coc-go
+# :CocInstall coc-pyright
+# :CocInstall coc-rust-analyzer
+
+
