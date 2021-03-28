76fe58dab68ef0b6fe54306539b3997e7c9e08d0
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2021-03-28 14:23:45 +0300

    feat: use dunst as notification daemon, update pacman,

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 cp ~/.config/polybar/config polybar/config
 cp ~/.config/bspwm/bspwmrc bspwm/bspwmrc
 cp ~/.config/sxhkd/sxhkdrc sxhkd/sxhkdrc
-cp ~/.zshrc .zshrc
 cp ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
+cp ~/.config/dunst/dunstrc dunst/dunstrc
 cp ~/.vimrc vim/.vimrc
+cp ~/.zshrc .zshrc
 pacman -Q > pacman/installed.txt
 
