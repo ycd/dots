91684aea5bb13a20fd94fdb55ea9d6ad6bd374d7
Yağız can Değirmenci <yagizcanilbey1903@gmail.com>
2021-05-01 12:12:21 +0300

    chore: unpack files from folders

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cp ~/.config/alacritty/alacritty.yml alacritty/alacritty.yml
-cp ~/.config/nvim/init.vim vim/init.vim
+cp ~/.config/nvim/init.vim init.vim
 cp ~/.zshrc .zshrc
 cp ~/.zsh_functions.zsh .zsh_functions.zsh
 cp ~/.zsh_aliases.zsh .zsh_aliases.zsh
