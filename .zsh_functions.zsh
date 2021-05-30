6dc15b5d7c29a59dd8b9d64d92e3e039a1064d3a
Yağız can Değirmenci <yagizcanilbey1903@gmail.com>
2021-05-30 16:26:13 +0300

    recent updates

--- .zsh_functions.zsh
+++ .zsh_functions.zsh
@@ -1,19 +1,27 @@
+function kubectl() {
+  if ! type __start_kubectl >/dev/null 2>&1; then
+      source <(command kubectl completion zsh)
+  fi
 
-# History with FZF
+  command kubectl "$@"
+}
+
 function hist() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
-# kubectl with autocomplete
-function kubectl() {
-    if ! type __start_kubectl >/dev/null 2>&1; then
-        source <(command kubectl completion zsh)
-    fi
-
-    command kubectl "$@"
+function bpl() {
+ : | fzf \
+  --reverse \
+  --preview='bash -c {q}' \
+  --preview-window=down:60 \
+  --print-query \
+  --color=dark \
+  --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
+  --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
 }
 
-# git add with fzf
+
 function gaf() {
-  git add . --dry-run | awk '{print $2}' | fzf --multi  | xargs git add 
+  git add --dry-run . | awk '{print $2}' | sed s/\'//g | fzf --reverse --preview='git diff {+}' --multi | xargs git add 
 }
