1408fe5d6de1f402ba0420b8362fc420add3904b
Yağız can Değirmenci <yagizcanilbey1903@gmail.com>
2021-05-30 20:13:37 +0300

    chore: update zsh_functions

--- .zsh_functions.zsh
+++ .zsh_functions.zsh
@@ -6,22 +6,31 @@ function kubectl() {
   command kubectl "$@"
 }
 
+
 function hist() {
   print -z $( ([ -n "$ZSH_NAME" ] && fc -l 1 || history) | fzf | sed -E 's/ *[0-9]*\*? *//' | sed -E 's/\\/\\\\/g')
 }
 
+
 function bpl() {
- : | fzf \
+ print -z $(: | fzf \
   --reverse \
   --preview='bash -c {q}' \
   --preview-window=down:60 \
-  --print-query \
-  --color=dark \
-  --color=fg:-1,bg:-1,hl:#5fff87,fg+:-1,bg+:-1,hl+:#ffaf5f \
-  --color=info:#af87ff,prompt:#5fff87,pointer:#ff87d7,marker:#ff87d7,spinner:#ff87d7
+  --print-query )
 }
 
 
 function gaf() {
-  git add --dry-run . | awk '{print $2}' | sed s/\'//g | fzf --reverse --preview='git diff {+}' --multi | xargs git add 
+  git add --dry-run . | awk '{print $2}' | sed s/\'//g | fzf --reverse --preview='git diff --color=always {+}' --multi | xargs git add 
+}
+
+
+function gsw() {
+  branch=$( git branch -l | fzf | sed s/\ //g )
+  without_prefix=$(startswith $branch "*")
+  if [[ $? -eq 0 ]]; then 
+    branch=$without_prefix
+  fi 
+  git switch $branch
 }
