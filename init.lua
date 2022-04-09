7d033d877e57e79b7835c76e336324d47af0b840
Yagiz Degirmenci <yagizcanilbey1903@gmail.com>
2022-04-09 15:57:16 +0300

    recent changes

--- init.lua
+++ init.lua
@@ -9,7 +9,10 @@ require'nvim-treesitter.configs'.setup {
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
-
-    additional_vim_regex_highlighting = true,
   },
+
+  indent = {
+    enable = false
+  },
+  
 }
